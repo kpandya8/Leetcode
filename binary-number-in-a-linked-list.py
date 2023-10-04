b2993bbdd9e2a7450227006a7329d0980779cd9b
Krutik Pandya <112588253+kpandya8@users.noreply.github.com>
2023-10-03 18:56:20 -0700

    Practise Questions Hackerrank

--- binary-number-in-a-linked-list.py
+++ binary-number-in-a-linked-list.py
@@ -35,52 +35,6 @@ def print_singly_linked_list(node, sep, fptr):
         if node:
             fptr.write(sep)
 
-
-#
-# Complete the 'getNumber' function below.
-#
-# The function is expected to return a LONG_INTEGER.
-# The function accepts INTEGER_SINGLY_LINKED_LIST binary as parameter.
-#
-
-#
-# For your reference:
-#
-# SinglyLinkedListNode:
-#     int data
-#     SinglyLinkedListNode next
-#
-#
-
-class SinglyLinkedListNode:
-    def __init__(self, node_data):
-        self.data = node_data
-        self.next = None
-
-class SinglyLinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-
-    def insert_node(self, node_data):
-        node = SinglyLinkedListNode(node_data)
-
-        if not self.head:
-            self.head = node
-        else:
-            self.tail.next = node
-
-        self.tail = node
-
-def print_singly_linked_list(node, sep, fptr):
-    while node:
-        fptr.write(str(node.data))
-
-        node = node.next
-
-        if node:
-            fptr.write(sep)
-
 # Complete the 'getNumber' function below.
 # The function is expected to return a LONG_INTEGER.
 # The function accepts INTEGER_SINGLY_LINKED_LIST binary as a parameter.
@@ -110,3 +64,5 @@ if __name__ == '__main__':
     fptr.write(str(result) + '\n')
 
     fptr.close()
+
+#TC: O(n); SC: O(n)
