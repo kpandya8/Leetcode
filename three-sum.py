04e46a8b709b4d3c7f2bc772d53cb5eb253daef9
Krutik Pandya <112588253+kpandya8@users.noreply.github.com>
2024-03-11 22:57:57 -0700

    optimised 3 sum

--- three-sum.py
+++ three-sum.py
@@ -31,18 +31,23 @@
 # 3 <= nums.length <= 3000
 # -105 <= nums[i] <= 105
 
-
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         ans=set()
         nums.sort()
         n=len(nums)
         for i in range(n-2):
-            for j in range(i+1,n-1):
-                for k in range(j+1,n):
-                    temp=nums[i]+nums[j]+nums[k]
-                    if temp==0:
-                        ans.add((nums[i],nums[j],nums[k]))
+            j=i+1
+            k=n-1
+            while j<k:
+                temp=nums[i]+nums[j]+nums[k]
+                if temp==0:
+                    ans.add((nums[i],nums[j],nums[k]))
+                    j+=1
+                elif temp>0:
+                    k-=1
+                else:
+                    j+=1
         return ans
     
 sol = Solution()
