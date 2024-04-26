9d7a384ec4c28e4bfe3f6b5b6a93bb526222b79b
Krutik Pandya <112588253+kpandya8@users.noreply.github.com>
2024-04-25 23:34:21 -0700

    Remove Nth Node from the List

--- remove-nth-node-from-list.py
+++ remove-nth-node-from-list.py
@@ -20,5 +20,51 @@
 # 0 <= Node.val <= 100
 # 1 <= n <= sz
 
-from typing import List as L
+from typing import List 
+from typing import Optional
+
+class ListNode:
+    def __init__(self, value = 0, next = None):
+        self.val = value
+        self.next = next
+
+class Solution:
+    def removeNthnode(self, head: Optional[ListNode], n:int) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        dummy.next = head
+        first = dummy
+        second = dummy
+
+        for _ in range(n+1):
+            first = first.next
+        
+        while first is not None:
+            first = first.next
+            second = second.next
+        
+        second.next = second.next.next
+
+        return dummy.next
+    
+
+def create_linked_list(elements):
+    head = ListNode(elements[0])
+    current = head
+    for element in elements[1:]:
+        current.next = ListNode(element)
+        current = current.next
+    return head
+
+sol = Solution()
+L = create_linked_list([1,2,3,4,5,6,7,8,9])
+
+def print_linked_list(head):
+    while head:
+        print(head.val, end=" -> ")
+        head = head.next
+    print("None")
+
+print_linked_list(sol.removeNthnode(L,2))
+
+
 
