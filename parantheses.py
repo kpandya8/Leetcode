90c22267410b2a567a6bc5d46e1a97297c3ed630
Krutik Pandya <112588253+kpandya8@users.noreply.github.com>
2024-04-26 23:11:41 -0700

    Parentheses for n.

--- parantheses.py
+++ parantheses.py
@@ -14,4 +14,25 @@
 
 # Input: n = 1
 # Output: ["()"]
- 
+
+from typing import List
+
+class Solution:
+    def parentheses(self, n:int)->List[str]:
+        output = []
+
+        def rec(left, right, stack, cand):
+            if left == right == 0:
+                output.append(cand)
+                return
+
+            if left > 0:
+                rec(left-1, right,stack+1,cand+"(")
+            if right > 0 and stack > 0:
+                rec(left, right-1, stack-1,cand+")")
+            
+        rec(n,n,0,"")
+
+        return output
+sol = Solution()
+print(sol.parentheses(4))
