bac350819ec8fd8d822b90f2b7ae207ca5db3372
Krutik Pandya <112588253+kpandya8@users.noreply.github.com>
2024-04-28 23:52:51 -0700

    Reverse Linked List Function

--- reverse-nodes-in-k-group.py
+++ reverse-nodes-in-k-group.py
@@ -30,3 +30,59 @@
  
 
 # Follow-up: Can you solve the problem in O(1) extra memory space?
+
+from typing import List, Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        dummy = prevEnd = ListNode(next=head)
+        curr = currEnd = head
+        prev = None
+        cnt = 1
+        while curr:
+            curr.next,prev,curr = prev,curr,curr.next
+            if cnt%k==0:
+                prevEnd.next,prevEnd,currEnd,prev = prev,currEnd,curr,None
+            cnt += 1
+
+        curr = prev
+        prev = None
+        while curr:
+            curr.next,prev,curr = prev,curr,curr.next
+        prevEnd.next = prev
+
+        return dummy.next
+    def get_kth_node(self, start, k):
+        while k > 0 and start:
+            start = start.next
+            k -= 1
+        return start
+
+def list_to_linked_list(elements):
+    dummy = ListNode(0)
+    current = dummy
+    for element in elements:
+        current.next = ListNode(element)
+        current = current.next
+    return dummy.next
+
+def linked_list_to_list(head):
+    elements = []
+    while head:
+        elements.append(head.val)
+        head = head.next
+    return elements
+
+# Example Usage
+elements = [1, 2, 3, 4, 5]
+k = 2
+head = list_to_linked_list(elements)
+sol = Solution()
+result_head = sol.reverseKGroup(head, k)
+result = linked_list_to_list(result_head)
+print(result)
